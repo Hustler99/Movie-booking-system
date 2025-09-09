41fb141d1dc58ba9fdbc523e9cb37008f7f3c0d0
Hustler <108349263+Hustler99@users.noreply.github.com>
2025-09-09 09:24:26 +0300

    Delete dist directory

--- dist/middlewares/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logger = ((req, res, next) => {
-    console.log(`${req.method}, ${req.protocol} , ${req.originalUrl}`);
-    next();
-});
-exports.default = logger;
